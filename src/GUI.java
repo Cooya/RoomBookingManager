4532fb8e04781374f4f65b8d239f498325bd32ce
Nicolas <nicolas.marcy@etu.u-bordeaux.fr>
2017-05-09 00:52:47 +0100

    login dialog box

---
+++ src/GUI.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class GUI extends Application {
+	
+	public static void display() {
+		launch();
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		new LoginDialog().display(stage);
+	}
+}
